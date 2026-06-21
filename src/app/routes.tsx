77bc6d4dae48f5d4677d29abc5c756265324781c
zukkata93-hue <zukkata93@gmail.com>
2026-06-21 15:05:17 +0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router"; 
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import Cart from "./pages/Cart";
@@ -7,7 +7,7 @@ import Help from "./pages/Help";
 import Checkout from "./pages/Checkout";
 import Layout from "./components/Layout";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([ 
   {
     path: "/",
     Component: Layout,
